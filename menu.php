01add1ef135e64cd829a22cc428ed9b380a5cdeb
William Hsu <will.s.hsu@gmail.com>
2016-02-12 21:12:44 -0800

    Added temporary bakery menu

--- menu.php
+++ menu.php
@@ -23,6 +23,7 @@ require_once("header.php");
 						</ul>
 					</div>
 					<div class="9u">
+					<!--
 						<ul class="actions small bakery" style="text-align: center;">
 							<li><a href="#" class="subcata button small special pastries">Pastries</a></li>
 							<li><a href="#" class="subcata button small special breads">Breads</a></li>
@@ -42,6 +43,88 @@ require_once("header.php");
 			              <li><a href="#" class="subcata button small special entrees">Entrees</a></li>
 			              <li><a href="#" class="subcata button small special desserts">Desserts</a></li>
 			            </ul>
+			        -->
+			        <h3>Bakery</h3>
+			        <pre>
+Breads
+	Country
+	Whole Grain
+	Rye
+	Cinnamon Raisin Swirl
+	Sourdough
+	Asiago Cheese
+	Tomato Basil
+	Honey Wheat
+	French Baguette
+	White
+	Asiago Cheese Focaccia
+	Sea Salt Focaccia
+	Ciabatta
+Bagels
+	Blueberry Bagel
+	Everything Bagel
+	French Toast Bagel
+	Asiago Cheese Bagel
+	Cinnamon Crunch Bagel
+	Whole Grain Bagel
+	Chocolate Chip Bagel
+	Cinnamon Swirl & Raisin Bagel
+	Plain Bagel
+	Sesame Bagel
+	Sprouted Grain Bagel Flat
+Pastries
+	French Croissant
+	Bear Claw
+	Cheese Pastry
+	Cherry Pastry
+	Chocolate Pastry
+	Pecan Braid
+	Pastry Ring
+Cookies
+	Semi Sweet Chocolate Chunk
+	Milk Chocolate Chip
+	Black & White
+	Dark Chocolate Walnut
+	Oatmeal Wheatgerm Chocolate Chip
+	Peanut Butter
+	Peanut Butter Chocolate Chunk
+	Oatmeal Raisin
+Croissants
+	Butter Croissant
+	Ham & Cheddar
+	Spinach & 3 Cheese
+	Turkey & Swiss
+	Blueberry Cream Cheese
+	Cranberry Cream Cheese
+	Apple Cream Cheese
+	Chocolate Cream Cheese
+Muffins
+	Blueberry Crumble Muffin
+	Black Bottom
+	Oatmeal Banana Muffin
+	Carrot Raisin Bran Muffin
+	Pumpkin Walnut Muffin
+Sweet Rolls & Buns
+	Cinnamon Roll
+	Sticky Bun
+	Cinnamon Sugar Morning Bun
+	Chocolate Almond Morning Bun
+Quiches
+	Diced Ham
+	Roasted Veggie
+	3 Cheese
+Scones
+	Blueberry Scone
+	Cranberry Scone
+	Almond Scone
+	Cappuccino Scone
+Coffee Cakes
+	Blueberry Coffee Cake
+	Fuji Apple Coffee Cake
+	Peach Coffee Cake
+	Cranberry Coffee Cake
+			        </pre>
+			        <h3>Restaurant</h3>
 					</div>
 				</div>
 			</div>
