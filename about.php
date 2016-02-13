a2ea2197fe3f7e5c2069c31bd339bbe0c0ad654d
turtlelee <scott.lee.3898@gmail.com>
2016-02-12 21:06:20 -0800

    Fixed about page photo

--- about.php
+++ about.php
@@ -4,7 +4,7 @@ require_once("header.php");
 
 <!-- Wrapper -->
 <section id="wrapper">
-	<header id="headerHome">
+	<header id="headerAbout">
 		<div class="inner">
 			<h2>About Panettiere</h2>
 			<p>Learn more about our passion and why we do what we do</p>
@@ -15,9 +15,11 @@ require_once("header.php");
 		<div class="wrapper">
 			<div class="inner">
 				<h3 class="major">Our mission</h3>
-				<p>As a modern-day bakery and restaurant, we strive to provide quality, healthy, and wholesome food for everyone. Finding a reliable source of gluten-free foods, let alone cooking completely gluten-free, can be extremely tough. As experts in the food industry, we know that maintaining a gluten-free lifestyle is challenging. That’s why we founded Panettiere Bakery &amp; Restaurant to provide both delicious food and inspiration to all.</p>
+				<p> As a modern-day bakery and restaurant, we strive to provide quality, healthy, and wholesome food for everyone. Finding a reliable source of gluten-free foods, let alone cooking completely gluten-free, can be extremely tough. As experts in the food industry, we know that maintaining a gluten-free lifestyle is challenging. That’s why we founded Panettiere Bakery &amp; Restaurant to provide both delicious food and inspiration to all.</p>
 
-				<p>At Panettiere, we uphold this standard, and more. We bring high-quality food to the table without neglecting the various dietary needs needs in today's society. By obtaining all of our ingredients fresh from local farmers and producers daily, we ensure the highest quality, as well as purity, of our food. Our bakers and chefs take extreme caution in preparing every pastry and dish that we serve in order to maintain our high standards.  </p>
+				<p> At Panettiere, we uphold this standard, and more. We bring high-quality food to the table without neglecting the various dietary needs needs in today's society. By obtaining all of our ingredients fresh from local farmers and producers daily, we ensure the highest quality, as well as purity, of our food. Our bakers and chefs take extreme caution in preparing every pastry and dish that we serve in order to maintain our high standards. </p>
+
+				<p> Excited about our food yet? Check out our menu here! </p>
 			</div>
 		</div>
 </section>
