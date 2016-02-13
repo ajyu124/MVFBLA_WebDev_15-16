dbe83c8f1928d4cf9114c3f1743138d7fb796c10
William Hsu <will.s.hsu@gmail.com>
2016-02-12 22:40:14 -0800

    Fixed reviews

--- reviews.php
+++ reviews.php
@@ -9,75 +9,76 @@ require_once("header.php");
 		</div>
 	</header>
 	
-		<div class="wrapper">
-			<div class="inner">
-				<section class="features">
-						<h3 class = "major">Awards and Recognition</h3>
-						<p>Despite being a relatively new bakery and restaurant, Panettiere Bakery has received recognition from multiple sources. Not only
-						are we a Grade A Restaurant in terms of service and cleanliness per Santa Clara County Laws, but we are an Occupational Health and 
-						Safety Association (OSHA) recognized establishment for our service and safety to employees and customers. Furthermore, we have 
-						also received a couple of awards which you can see below!</p>
-						<article>
-							<h3 class="major">Best New Bakery - American Bakers Assocation 2016 </h3>
-							<p>We are proud to announce that we received the "Best New Bakery Award" from the American Bakers Association for our 
-							food and service to our customers. We thank the American Bakers Assocation greatly for this award.</p>
+	<div class="wrapper">
+		<div class="inner">
+			<section class="features">
+				<h3 class = "major">Awards and Recognition</h3>
+				<p>Despite being a relatively new bakery and restaurant, Panettiere Bakery has received recognition from multiple sources. Not only
+					are we a Grade A Restaurant in terms of service and cleanliness per Santa Clara County Laws, but we are an Occupational Health and 
+					Safety Association (OSHA) recognized establishment for our service and safety to employees and customers. Furthermore, we have 
+					also received a couple of awards which you can see below!</p>
+				<article>
+					<h3 class="major">Best New Bakery - American Bakers Assocation 2016 </h3>
+					<p>We are proud to announce that we received the "Best New Bakery Award" from the American Bakers Association for our 
+					food and service to our customers. We thank the American Bakers Assocation greatly for this award.</p>
+				</article>
+				<article>
+					<h3 class="major">Best Service - James Beard Restaurant Awards 2016</h3>
+					<p>We are also proud to announce that we received the Best Service Award from the James Beard Restaurant Awards! We work hard to
+					make sure that each individual customer has a great time at our establishment and we thank the James Beard Awards for this 
+					recognition.</p>
+				</article>
+			</section> 
+			<section>
+				<h3 class = "major">Customer Reviews</h3>
+				<h4>"Astounding gluten-free food"</h4>
+				<blockquote>It's not very easy to find gluten free restaurants and bakeries today, and it's even harder to find ones that have 
+					good tasting food as well! I can safely say that I have found <i>the best</i> gluten free restaurant and bakery in the Bay Area, 
+					and perhaps even California. The pastries are amazing and the entrees are even better. Their customer service is stellar and they
+					have really found a unique way to merge with technology through their mobile stamp card. I fully recommend that everyone visit this bakery.
+					<br><br>
+					- Mark C, Santa Clara, CA
+				</blockquote>
+				<p></p>
 						
-						</article>
-						<article>
-							<h3 class="major">Best Service - James Beard Restaurant Awards 2016</h3>
-							<p>We are also proud to announce that we received the Best Service Award from the James Beard Restaurant Awards! We work hard to
-							make sure that each individual customer has a great time at our establishment and we thank the James Beard Awards for this 
-							recognition.</p>
+				<h4>"I have never been disappointed"</h4>
+				<blockquote>I cannot say enough about this bakery! I've been coming here for a couple months now and have never been disappointed! 
+						Everything always tastes amazing and looks just as good as it tastes. However, today I'm specifically writing because 
+						of the service I received. I was ordering at the last minute, having to go back and forth with my committee for a project 
+						on what to order and had a special request for them to accommodate our schedule and without hesitation, they were able
+						to deliver! Like I said, I've been a customer for many years and will continue coming back and referring my family 
+						and friends because they've earned the business! Keep up the great work!! <br><br> 
 						
-						</article>
-					</section> 
-					<section>
-						<h3 class = "major">Customer Reviews</h3>
-						<h4>"Astounding gluten free food"</h4>
-							<blockquote>It's not very easy to find gluten free restaurants and bakeries today, and it's even harder to find ones that have 
-							good tasting food as well! I can safely say that I have found <i>the best</i> gluten free restaurant and bakery in the Bay Area, 
-							and perhaps even California. The pastries are amazing and the entrees are even better. Their customer service is stellar and they
-							have really found a unique way to merge with technology through their mobile stamp card. I fully recommend that everyone visit this bakery.
-							<br><br>
-							- Mark C, Santa Clara, CA
-							</blockquote>
-							<p></p>
-							
-						<h4>"I have never been disappointed"</h4>
-							<blockquote>I cannot say enough about this bakery! I've been coming here for a couple months now and have never been disappointed! 
-							Everything always tastes amazing and looks just as good as it tastes. However, today I'm specifically writing because 
-							of the service I received. I was ordering at the last minute, having to go back and forth with my committee for a project 
-							on what to order and had a special request for them to accommodate our schedule and without hesitation, they were able
-							to deliver! Like I said, I've been a customer for many years and will continue coming back and referring my family 
-							and friends because they've earned the business! Keep up the great work!! <br><br> 
-							
-							- Jon S, San Jose, CA
-							</blockquote>
-							
-						<h4>"Customer Service is great!!"</h4>
-							<blockquote>Amazing. ..the people are all about Customer service and the treats look delicious... I can  hardly wait 
-							to bite into my chocolate croissant...my blueberry muffin and my crunchy oatmeal cookie. My only problem  is which to
-							eat first! Oh my mouth is watering! <br><br>
-							
-							- Jane C, San Francisco, CA
-							</blockquote>
+						- Jon S, San Jose, CA
+				</blockquote>
 						
-							
-						<h4>"Just. Delicious."</h4>
-							<blockquote>Panettiere's is delish! I literally bought one of everything for my offices and they all loved them. I got croissants for my
-							self and they seriously taste better than ones that they sell at professional bakeries. Even my boss agrees with me! So amazing. I'll be 
-							coming back for sure. <br><br> 
-							
-							- Kavita G, Saratoga, CA
-							</blockquote>
+				<h4>"Customer Service is great!!"</h4>
+				<blockquote>Amazing. ..the people are all about Customer service and the treats look delicious... I can  hardly wait 
+						to bite into my chocolate croissant...my blueberry muffin and my crunchy oatmeal cookie. My only problem  is which to
+						eat first! Oh my mouth is watering! <br><br>
 						
-						<h4>"Best bakery in the Bay Area!"</h4>
-							<blockquote>Excellent service! Excellent food! Wonderful pastries with a very cozy atmosphere. The only set back 
-							is that there was a long line and wait around dinner time which was slightly annoying. Overall, an excellent place to eat and very 
-							inexpensive too! <br><br>
-							- Guillermo F, Cupertino, CA
-							</blockquote>
-							
-						</section> 
-					</section>	
-		
+						- Jane C, San Francisco, CA
+				</blockquote>
+					
+						
+				<h4>"Just. Delicious."</h4>
+				<blockquote>Panettiere's is delish! I literally bought one of everything for my offices and they all loved them. I got croissants for my
+						self and they seriously taste better than ones that they sell at professional bakeries. Even my boss agrees with me! So amazing. I'll be 
+						coming back for sure. <br><br> 
+						
+						- Kavita G, Saratoga, CA
+				</blockquote>
+					
+				<h4>"Best bakery in the Bay Area!"</h4>
+				<blockquote>Excellent service! Excellent food! Wonderful pastries with a very cozy atmosphere. The only set back 
+						is that there was a long line and wait around dinner time which was slightly annoying. Overall, an excellent place to eat and very 
+						inexpensive too! <br><br>
+						- Guillermo F, Cupertino, CA
+				</blockquote>			
+			</section> 
+		</div>
+	</div>
+</section>
+<?php
+require_once("footer.php");
+?>	
