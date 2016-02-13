44d5e5a244b10faf85e02aee05464fe4a0bf7745
William Hsu <will.s.hsu@gmail.com>
2016-02-12 19:59:57 -0800

    Added barebones menu

---
+++ menu.php
@@ -0,0 +1,51 @@
+<?php
+require_once("header.php");
+?>
+
+<!-- Wrapper -->
+<section id="wrapper">
+	<header id="header">
+		<div class="inner">
+			<h2>Menu</h2>
+			<!--<p>A modern bakery and restaurant with hearty, gluten-free food for everyone</p>-->
+		</div>
+	</header>
+
+	<!-- Content -->
+		<div class="wrapper">
+			<div class="inner">
+				<div class="row">
+					<div class="3u" style="padding-top: 30px;"><h4>Menus</h4>
+						<ul class="alt">
+							<li style="background-color: rgba(255, 255, 255, 0.025);"><a href="#">All</a></li>
+							<li><a href="#" class="cata bakery">Bakery</a></li>
+							<li><a href="#" class="cata restaurant">Restaurant</a></li>
+						</ul>
+					</div>
+					<div class="9u">
+						<ul class="actions small bakery" style="text-align: center;">
+							<li><a href="#" class="subcata button small special pastries">Pastries</a></li>
+							<li><a href="#" class="subcata button small special breads">Breads</a></li>
+							<li><a href="#" class="subcata button small special bagels">Bagels</a></li>
+							<li><a href="#" class="subcata button small special croissants">Croissants</a></li>
+							<li><a href="#" class="subcata button small special muffins">Muffins</a></li>
+							<li><a href="#" class="subcata button small special rollsbuns">Rolls & Buns</a></li>
+							<li><a href="#" class="subcata button small special scones">Scones</a></li>
+							<li><a href="#" class="subcata button small special coffeecakes">Coffee Cakes</a></li>
+						</ul>
+						<ul class="actions small restaurant" style="text-align: center;">
+			              <li><a href="#" class="subcata button small special appetizers">Appetizers</a></li>
+			              <li><a href="#" class="subcata button small special soups">Soups</a></li>
+			              <li><a href="#" class="subcata button small special salads">Salads</a></li>
+			              <li><a href="#" class="subcata button small special pastas">Pastas</a></li>
+			              <li><a href="#" class="subcata button small special entrees">Entrees</a></li>
+			              <li><a href="#" class="subcata button small special desserts">Desserts</a></li>
+			            </ul>
+					</div>
+				</div>
+			</div>
+		</div>
+</section>
+
+<?php
+require_once("f
