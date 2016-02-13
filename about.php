7d0e66f2d08551173fe38e6c915fc6c1abf622b3
turtlelee <scott.lee.3898@gmail.com>
2016-02-12 21:10:48 -0800

    Updated link in about

--- about.php
+++ about.php
@@ -19,7 +19,7 @@ require_once("header.php");
 
 				<p> At Panettiere, we uphold this standard, and more. We bring high-quality food to the table without neglecting the various dietary needs needs in today's society. By obtaining all of our ingredients fresh from local farmers and producers daily, we ensure the highest quality, as well as purity, of our food. Our bakers and chefs take extreme caution in preparing every pastry and dish that we serve in order to maintain our high standards. </p>
 
-				<p> Excited about our food yet? Check out our menu here! </p>
+				<p> Excited about our food yet? Check out our menu <a href="menu.php">here!</a></p>
 			</div>
 		</div>
 </section>
