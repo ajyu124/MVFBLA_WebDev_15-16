57a6b2f105fab7f34ce5eff15bc364e6eeb15ea0
sidsatya <sidsatya74@gmail.com>
2016-02-13 02:13:58 +0000

    something

---
+++ reviews.php
@@ -0,0 +1,13 @@
+<?php
+require_once("header.php");
+?>
+						
+		<div class="wrapper">
+				<div class="inner">
+					<section>
+						<h4>"Best bakery in the Bay Area!"</h4>
+							<blockquote>Excellent service! Excellent food! Wonderful pastries with a very cozy atmosphere. The only set back 
+							is that there was a long line and wait which was slightly annoying. Overall, an excellent place to eat and very 
+							inexpensive too!
+							</blockquote>
+							<p>- Guillermo Ferrero, Cupertino, CA</p>
