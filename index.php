605a50c233f7b7427f4ecbdf06629467590698fd
turtlelee <scott.lee.3898@gmail.com>
2016-02-12 17:39:22 -0800

    Updated home page description

--- index.php
+++ index.php
@@ -14,14 +14,9 @@ require_once("header.php");
 	<!-- Content -->
 		<div class="wrapper">
 			<div class="inner">
-
-				<h3 class="major">Lorem ipsum dolor</h3>
-				<p>Morbi mattis mi consectetur tortor elementum, varius pellentesque velit convallis. Aenean tincidunt lectus auctor mauris maximus, ac scelerisque ipsum tempor. Duis vulputate ex et ex tincidunt, quis lacinia velit aliquet. Duis non efficitur nisi, id malesuada justo. Maecenas sagittis felis ac sagittis semper. Curabitur purus leo donec vel dolor at arcu tincidunt bibendum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce ut aliquet justo. Donec id neque ipsum. Integer eget ultricies odio. Nam vel ex a orci fringilla tincidunt. Aliquam eleifend ligula non velit accumsan cursus. Etiam ut gravida sapien.</p>
-
-				<p>Vestibulum ultrices risus velit, sit amet blandit massa auctor sit amet. Sed eu lectus sem. Phasellus in odio at ipsum porttitor mollis id vel diam. Praesent sit amet posuere risus, eu faucibus lectus. Vivamus ex ligula, tempus pulvinar ipsum in, auctor porta quam. Proin nec commodo, vel scelerisque nisi scelerisque. Suspendisse id quam vel tortor tincidunt suscipit. Nullam auctor orci eu dolor consectetur, interdum ullamcorper ante tincidunt. Mauris felis nec felis elementum varius.</p>
-
-				<h3 class="major">Vitae phasellus</h3>
-				<p>Cras mattis ante fermentum, malesuada neque vitae, eleifend erat. Phasellus non pulvinar erat. Fusce tincidunt, nisl eget mattis egestas, purus ipsum consequat orci, sit amet lobortis lorem lacus in tellus. Sed ac elementum arcu. Quisque placerat auctor laoreet.</p>
+				<h3 class="major">Our mission</h3>
+				<p>As a modern-day bakery and restaurant, we strive to provide quality, healthy, and wholesome food for everyone. As experts in the food industry, we know that maintaining a gluten-free lifestyle can be challenging. At Panettiere, we uphold this standard, and more. We bring high-quality food to the table without neglecting the various dietary needs needs in today's society. Learn more about our philosophy below. </p>
+				<a href="#" class="special">Read more</a>
 
 				<section class="features">
 					<article>
