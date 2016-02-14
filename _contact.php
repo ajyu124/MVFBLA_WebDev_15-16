eaed9aebaaa9bf4d0cff04659eb15545530db960
William Hsu <will.s.hsu@gmail.com>
2016-02-13 20:24:27 -0800

    Contact form database

--- _contact.php
+++ _contact.php
@@ -3,7 +3,17 @@ $name = $_POST["name"];
 $email = $_POST["email"];
 $message = $_POST["message"];
 
-echo $name . "\n";
-echo $email . "\n";
-echo $message . "\n";
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+if ($conn->connect_errno) { /*fail quietly*/ }
+else {
+	$conn->query("INSERT INTO messages (name, email, message) VALUES ('$name', '$email', '$message')");
+}
+$conn-close();
 ?>
