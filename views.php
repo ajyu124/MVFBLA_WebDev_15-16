32996cd839a13e3ffd817518c5396ecde23e193a
William Hsu <will.s.hsu@gmail.com>
2016-02-13 20:44:20 -0800

    Fancy views

--- views.php
+++ views.php
@@ -1,9 +1,3 @@
-<html>
-<head>
-	<title>Views</title>
-	<link rel="icon" href="images/favicon.ico">
-</head>
-<body>
 <?php
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $server = $url["host"];
@@ -16,32 +10,61 @@ if ($conn->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 }
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Panettiere | Loaves of Love</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<link rel="icon" href="images/favicon.ico">
+	</head>
+	<body>
 
-if ($result = $conn->query("SELECT * FROM `views` ORDER BY `id` DESC")) {
-    printf("%d logged pageviews.\n", $result->num_rows);
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
 
-    echo "<table border=1>";
-	echo "<tr><th>id</th><th>time</th><th>ip</th><th>useragent</th><th>page</th></tr>";
-	while ($row = $result->fetch_array()) {
-		echo "<tr>";
-		echo "<td>" . $row['id'] . "</td>";
-		echo "<td>" . $row['time'] . "</td>";
-		echo "<td>" . $row['ip'] . "</td>";
-		echo "<td>" . $row['useragent'] . "</td>";
-		echo "<td>" . $row['page'] . "</td>";
-		echo "</tr>";
-	}
-	echo "</table>";
-    /* free result set */
-    $result->close();
-}
-else {
-	echo $conn->error;
-}
-echo "<br><hr><br>";
+				<!-- Nav bar -->
+					<div id="nav">
+						<h1><a href="http://www.panettiere.ml/"><img src="images/logo3.png" class="logo"></a></h1>
+						<nav>
+							<a href="#menu">Pages</a>
+						</nav>
+					</div>
 
+				<!-- Menu -->
+					<nav id="menu">
+						<div class="inner">
+							<h2>Pages</h2>
+							<ul class="links">
+								<li><a href="/">Home</a></li>
+								<li><a href="/about.php">About Us</a></li>
+								<li><a href="/menu.php">Menu</a></li>
+								<li><a href="#footer">Reservations</a></li>
+								<li><a href="/rewards.php">Loyalty Program</a></li>
+								<li><a href="/reviews.php">Reviews</a></li>
+							</ul>
+							<a href="#" class="close">Close</a>
+						</div>
+					</nav>
+<section id="wrapper">
+	<header id="headerRewards">
+		<div class="inner">
+			<h2>Admin Panel</h2>
+			<p>View messages and page logs</p>
+		</div>
+	</header>
+
+	<!-- Content -->
+		<div class="wrapper">
+			<div class="inner">
+<?php
 if ($result = $conn->query("SELECT * FROM `messages` ORDER BY `id` DESC")) {
-    printf("%d messages.\n", $result->num_rows);
+    printf("<h3 class='major'>Messages (%d)</h3>", $result->num_rows);
 
     echo "<table border=1>";
 	echo "<tr><th>id</th><th>time</th><th>name</th><th>email</th><th>message</th></tr>";
@@ -61,7 +84,35 @@ if ($result = $conn->query("SELECT * FROM `messages` ORDER BY `id` DESC")) {
 else {
 	echo $conn->error;
 }
+?>
+				<br><hr><br>
+<?php
+if ($result = $conn->query("SELECT * FROM `views` ORDER BY `id` DESC")) {
+    printf("<h3 class='major'>Pageviews (%d)</h3>", $result->num_rows);
 
+    echo "<table border=1>";
+	echo "<tr><th>id</th><th>time</th><th>ip</th><th>useragent</th><th>page</th></tr>";
+	while ($row = $result->fetch_array()) {
+		echo "<tr>";
+		echo "<td>" . $row['id'] . "</td>";
+		echo "<td>" . $row['time'] . "</td>";
+		echo "<td>" . $row['ip'] . "</td>";
+		echo "<td>" . $row['useragent'] . "</td>";
+		echo "<td>" . $row['page'] . "</td>";
+		echo "</tr>";
+	}
+	echo "</table>";
+    /* free result set */
+    $result->close();
+}
+else {
+	echo $conn->error;
+}
+?>
+			</div>
+		</div>
+</section>
+<?php
 $conn->close();
 ?>
 </body></html>
