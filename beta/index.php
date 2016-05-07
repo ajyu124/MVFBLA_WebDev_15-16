3ee98d189fb3daef2afb08503c3cf532e64c0c73
William Hsu <will.s.hsu@gmail.com>
2016-05-07 15:37:47 -0700

    added temporary window

--- beta/index.php
+++ beta/index.php
@@ -1,7 +1,12 @@
 <?php
 require_once("header.php");
 ?>
-Hello World
+<header class="window">
+	<div class="innerWindow">
+		<h2>Panettiere</h2>
+		<p>A modern bakery and restaurant with hearty, gluten-free food for everyone</p>
+	</div>
+</header>
 <?php
 require_once("footer.php");
 ?>
