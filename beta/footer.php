0fba28b08da2cae1b10e3f751d68353b90597591
William Hsu <will.s.hsu@gmail.com>
2016-05-07 13:55:21 -0700

    started working on beta

---
+++ beta/footer.php
@@ -0,0 +1,64 @@
+
+<!--
+<section id="footer" style="padding-top: 40px">
+	<div class="inner">
+		<h3 class="major">Want to check us out? Hours / Social / Reservations / Contact Us</h3>
+		<ul class="contact">
+			<p>Reservations can be made through the Yelp app using SeatMe, available on <a href="https://itunes.apple.com/us/app/yelp/id284910350?mt=8">iOS</a> and <a href="https://play.google.com/store/apps/details?id=com.yelp.android&hl=en">Android.</a> You can also call us at (408)-320-9455 at least 1 hour before your desired time.</p>
+			<li class="fa-home">
+				<a href="https://goo.gl/maps/pD5L1BYHgpE2">21840 McClellan Rd <br>
+						Cupertino, CA 95014</a> <br>
+						Mon - Fri: 5:30 AM - 2:00 PM | 5:00 PM - 9:30 PM <br>
+						Sat - Sun: 6:30 AM - 2:00 PM | 5:00 PM - 11:00 PM
+			</li>
+			<li class="fa-phone">(408)-320-9455</li>
+			<li class="fa-envelope"><a href="mailto:hello@panettiere.ml">hello@panettiere.ml</a></li>
+			<li class="fa-facebook"><a href="https://www.facebook.com/panettierebakery">/panettierebakery/</a></li>
+			<li class="fa-instagram"><a href="https://www.instagram.com/panettierebakery/">/panettierebakery/</a></li>
+		</ul>
+		<form id="form">
+			<p>If you have any questions about our mission, food, or anything else, please feel free to contact us directly through the form below, or via any of the listed social media. </p>
+			<div class="field">
+				<label for="name">Name (required)</label>
+				<input type="text" name="name" id="name" placeholder="Hi, what's your name?" required />
+			</div>
+			<div class="field">
+				<label for="email">Email (required)</label>
+				<input type="email" name="email" id="email" placeholder="How should we get back to you?" required />
+			</div>
+			<div class="field">
+				<label for="message">Message (required)</label>
+				<textarea name="message" id="message" rows="3" placeholder="What's on your mind?" required></textarea>
+			</div>
+			<ul class="actions">
+				<li><input type="submit" value="Send Message" /></li>
+			</ul>
+		</form>
+		<ul class="copyright">
+			<li>&copy; 2016 Panettiere Inc. All rights reserved.</li>
+			<li>This work and all works (including images) used in its creation are licensed under a <a href="http://creativecommons.org/licenses/by/4.0/">CCA 4.0 International License.</a></li>
+		</ul>
+	</div>
+</section>
+-->
+	<!-- Scripts -->
+	<script src="/assets/js/skel.min.js"></script>
+	<script src="/assets/js/jquery.min.js"></script>
+	<script src="/assets/js/jquery.scrollex.min.js"></script>
+	<script src="/assets/js/util.js"></script>
+	<script src="/assets/js/main.js"></script>
+	<!--<script src="/assets/js/contact.js"></script>-->
+	
+	<!--
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-49750866-5', 'auto');
+		ga('send', 'pageview');
+	</script>
+	-->
+</body>
+</html>
---
+++ beta/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Panettiere | Loaves of Love</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--<link rel="stylesheet" href="/assets/css/main.css" />-->
+		<link rel="stylesheet" href="style.css" />
+		<link rel="icon" href="/images/favicon.ico">
+	</head>
+	<body>
+			<div id="nav">
+				<h1><a href="http://www.panettiere.ml/"><img src="images/logo3.png" class="logo"></a></h1>
+				<nav>
+					<a href="#pages">Pages</a>
+				</nav>
+			</div>
+			<nav id="pages">
+				<div class="inner">
+					<h2>Pages</h2>
+					<ul class="links">
+						<li><a href="/">Home</a></li>
+						<li><a href="/about.php">About Us</a></li>
+						<li><a href="/menu.php">Menu</a></li>
+						<li><a href="/reservations.php">Reservations</a></li>
+						<li><a href="/rewards.php">Loyalty Program</a></li>
+						<li><a href="/reviews.php">Reviews</a></li>
+					</ul>
+					<a href="#" class="close">Close</a>
+				</div>
+			</nav>
