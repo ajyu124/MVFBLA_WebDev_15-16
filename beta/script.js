dce9d7be58b01a21dceee461c1de3f876e73a85a
William Hsu <will.s.hsu@gmail.com>
2016-05-07 16:23:10 -0700

    made transitions opacity-based

--- beta/script.js
+++ beta/script.js
@@ -1,17 +1,17 @@
 (function ($) {
 	var nav = false;
-	$("#pages").hide().css("opacity", 0);
+	$("#pages").css("opacity", 0).show();
 	$("#pages").click(function () {
-		$("#pages").css("opacity", 0).hide();
+		$("#pages").css("opacity", 0);
 		nav = false;
 	});
 	$(".close").click(function () {
-		$("#pages").css("opacity", 0).hide();
+		$("#pages").css("opacity", 0);
 		nav = false;
 	});
 	$("a[href='#pages']").click(function () {
 		var t = nav;
-		t = t ? $("#pages").css("opacity", 0).hide() : $("#pages").show().css("opacity", 1);
+		t = t ? $("#pages").css("opacity", 0) : $("#pages").css("opacity", 1);
 		nav = !nav;
 	});
 })($);
