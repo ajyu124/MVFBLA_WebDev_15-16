daa436774dd01d5202945c45e2ac2398fba98951
William Hsu <will.s.hsu@gmail.com>
2016-05-07 17:40:48 -0700

    reverted transitions

--- beta/script.js
+++ beta/script.js
@@ -1,17 +1,17 @@
 (function ($) {
 	var nav = false;
-	$("#pages").css("opacity", 0).show();
+	$("#pages").css("opacity", 0) && setTimeout(function () {$("#pages").css("display", "none");}, 350);
 	$("#pages").click(function () {
-		$("#pages").css("opacity", 0);
+		$("#pages").css("opacity", 0) && setTimeout(function () {$("#pages").css("display", "none");}, 350);
 		nav = false;
 	});
 	$(".close").click(function () {
-		$("#pages").css("opacity", 0);
+		$("#pages").css("opacity", 0) && setTimeout(function () {$("#pages").css("display", "none");}, 350);
 		nav = false;
 	});
 	$("a[href='#pages']").click(function () {
 		var t = nav;
-		t = t ? $("#pages").css("opacity", 0) : $("#pages").css("opacity", 1);
+		t = t ? $("#pages").css("opacity", 0) && setTimeout(function () {$("#pages").css("display", "none");}, 350) : $("#pages").css("display", "flex") && setTimeout(function () {$("#pages").css("opacity", 1);}, 1);
 		nav = !nav;
 	});
 })($);
