8a1a8d107c973db3bfc108a258888ff0573948a2
William Hsu <will.s.hsu@gmail.com>
2016-05-07 18:53:08 -0700

    added features

--- beta/index.php
+++ beta/index.php
@@ -11,6 +11,20 @@ require_once("header.php");
 	<h3>Our mission</h3>
 	<p>As a modern-day bakery and restaurant, we strive to provide quality, healthy, and wholesome food for everyone. As experts in the food industry, we know that maintaining a gluten-free lifestyle can be challenging. At Panettiere, we uphold this standard, and more. We bring high-quality food to the table without neglecting the various dietary needs needs in today's society. Learn more about our philosophy below.</p>
 	<a href="about.php" class="special">Read more</a>
+	<div class="row">
+		<div class="column six box">
+			<img src="/images/bakeryFeature.jpg" class="image"/>
+			<h3>Panettiere Bakery</h3>
+			<p>Start your morning with fresh bread and a warm cup of coffee, or freshen up your afternoon with a gourmet pastry. All gluten and wheat-free, of course.</p>
+			<a href="menu.php#breads" class="special">See our baked goods</a>
+		</div>
+		<div class="column six box">
+			<img src="/images/restaurantFeature.jpg" class="image">
+			<h3>Panettiere Restaurant</h3>
+			<p>Whether it's a quick lunch or a full dinner party, Panettiere Restaurant has got you covered with a variety of appealing dishes for everyone to enjoy.</p>
+			<a href="menu.php#restaurant" class="special">See our menu</a>
+		</div>
+	</div>
 </div>
 <?php
 require_once("footer.php");
