e6b967b1d46d499be75c821f91b5e1841aa07b02
William Hsu <will.s.hsu@gmail.com>
2016-05-07 18:41:24 -0700

    adding content

--- beta/index.php
+++ beta/index.php
@@ -1,12 +1,17 @@
 <?php
 require_once("header.php");
 ?>
-<header class="window">
+<header class="window" id="windowHome">
 	<div class="innerWindow">
 		<h2>Panettiere</h2>
 		<p>A modern bakery and restaurant with hearty, gluten-free food for everyone</p>
 	</div>
 </header>
+<div class="container">
+	<h3>Our mission</h3>
+	<p>As a modern-day bakery and restaurant, we strive to provide quality, healthy, and wholesome food for everyone. As experts in the food industry, we know that maintaining a gluten-free lifestyle can be challenging. At Panettiere, we uphold this standard, and more. We bring high-quality food to the table without neglecting the various dietary needs needs in today's society. Learn more about our philosophy below.</p>
+	<a href="about.php" class="special">Read more</a>
+</div>
 <?php
 require_once("footer.php");
 ?>
