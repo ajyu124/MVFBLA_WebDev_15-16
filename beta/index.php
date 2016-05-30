4c4095e98bbf9124439b82640d09b0da530f77e4
William Hsu <will.s.hsu@gmail.com>
2016-05-30 10:43:22 -0700

    restructure images

--- beta/index.php
+++ beta/index.php
@@ -17,16 +17,16 @@ require_once("header.php");
 	</div>
 	<div class="row">
 		<div class="columns six box">
-			<img src="/images/bakeryFeature.jpg" class="image"/>
+			<img src="/images/features/bakery.jpg" class="image"/>
 			<h3>Panettiere Bakery</h3>
 			<p>Start your morning with fresh bread and a warm cup of coffee, or freshen up your afternoon with a gourmet pastry. All gluten and wheat-free, of course.</p>
 			<a href="menu.php#breads" class="special">See our baked goods</a>
 		</div>
 		<div class="columns six box">
-			<img src="/images/restaurantFeature.jpg" class="image">
+			<img src="/images/features/restaurant.jpg" class="image">
 			<h3>Panettiere Restaurant</h3>
 			<p>Whether it's a quick lunch or a full dinner party, Panettiere Restaurant has got you covered with a variety of appealing dishes for everyone to enjoy.</p>
-			<a href="menu.php#restaurant" class="special">See our menu</a>
+			<a href="menu.php#entrees" class="special">See our menu</a>
 		</div>
 	</div>
 </div>
