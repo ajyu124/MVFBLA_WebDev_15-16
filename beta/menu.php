b9c5a850a91fe54a4a4f530393f303f635deb8c8
William Hsu <will.s.hsu@gmail.com>
2016-05-29 23:53:49 -0700

    raw menu

---
+++ beta/menu.php
@@ -0,0 +1,26 @@
+<?php
+require_once("header.php");
+?>
+<header class="window half" id="windowAbout">
+	<div class="container">
+		<h2>Menu</h2>
+		<p>A listing of our regular offerings</p>
+	</div>
+</header>
+<div class="container">
+	<div class="row">
+		<div class="columns twelve">
+			<p>Hungry <em>right now</em>? Order Panettiere food directly to your door with services like <a href="https://doordash.com">DoorDash</a>, who we are partnered with.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="columns three">
+			<p><a href="#restaurant" class="cata restaurant">Restaurant</a></p>
+			<p><a href="#bakery" class="cata bakery">Bakery</a></p>
+		</div>
+		<div class="columns nine"></div>
+	</div>
+</div>
+<?php
+require_once("footer.php");
+?>
--- beta/script.js
+++ beta/script.js
@@ -174,3 +174,21 @@
 		});
 	});
 })($);
+
+(function ($) {
+	$(".menu-item").hide();
+	$(".cata.bakery").click(function () { $(".subcata.restaurant").hide2().siblings(".bakery").showInst().first().children().click(); });
+	$(".cata.restaurant").click(function () { $(".subcata.bakery").hide2().siblings(".restaurant").showInst().first().children().click(); });
+	$(".subcata a").click(function () {
+		$(this).parent().siblings().children().removeClass("special");
+		$(this).addClass("special");
+		$(".menu-item:not(."+$(this).attr("href").substring(1)+")").hide2().siblings("."+$(this).attr("href").substring(1)).show2();
+	});
+	if (location.hash.substring(1) == "")
+		location.hash = "#breads";
+	if ($(".subcata." + location.hash.substring(1)).hasClass("bakery"))
+		$(".cata.bakery").click();
+	else
+		$(".cata.restaurant").click();
+	$(".subcata." + location.hash.substring(1)).click();
+})($);
