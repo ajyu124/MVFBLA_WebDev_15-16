b90044aaf756363c3eadc6568a242d1b116e488b
William Hsu <will.s.hsu@gmail.com>
2016-05-30 02:35:03 -0700

    minor menu tweaks

--- beta/menu.php
+++ beta/menu.php
@@ -10,7 +10,7 @@ require_once("header.php");
 <div class="container">
 	<div class="row">
 		<div class="columns twelve">
-			<p>Hungry <em>right now</em>? Order Panettiere food directly to your door with services like <a href="https://doordash.com">DoorDash</a>, who we are partnered with.</p>
+			<p>Hungry <em>right now</em>? Order food directly to your door with services like <a href="https://doordash.com">DoorDash</a>, a delivery service Panettiere is partnered with.</p>
 		</div>
 	</div>
 	<div class="row">
