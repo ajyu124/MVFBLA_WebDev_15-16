9e5e98a7a0cebbdb69a3863ae36524521b056111
William Hsu <will.s.hsu@gmail.com>
2016-05-30 11:46:38 -0700

    Style tweaks

--- 404.php
+++ 404.php
@@ -1,7 +1,7 @@
 <?php
 require_once("header.php");
 ?>
-<header class="window half" id="windowAbout">
+<header class="window half" id="windowHome">
 	<div class="container">
 		<h2>404 Not Found</h2>
 		<p>Error: Page not found</p>
@@ -11,7 +11,8 @@ require_once("header.php");
 	<div class="row">
 		<div class="columns twelve">
 			<p>The page you were looking for could not be found.</p>
-			<p>However, if you were looking for delicious, savory dishes to satisfy your hunger, you can look through <a href="menu.php">Panettiere's menu</a> to choose something that your heart desires.</p>
+			<p class="pushtop">However, if you were looking for delicious, savory dishes to satisfy your hunger, you can look through <a href="menu.php">Panettiere's menu</a> to choose something that your heart desires.</p>
+			<p class="pushtop">Still lost? Click the Panettiere logo to go to the <a href="/">home page</a>. Use the contact form below if you believe something should be here.</p>
 		</div>
 	</div>
 </div>
