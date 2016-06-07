81f28919a7db0cb1d853c99227d1274a8bf6ee6d
William Hsu <will.s.hsu@gmail.com>
2016-06-07 15:57:38 -0700

    mobile-friendly images

--- about.php
+++ about.php
@@ -11,11 +11,12 @@ require_once("header.php");
 	<div class="row">
 		<div class="columns twelve">
 			<h4>Our mission</h4>
-			<img src="images/features/about_1.png" class="img right">
-			<br><br>
+			<img src="images/features/about_1.png" class="img right hidesmall">
+			<br class="hidesmall"><br class="hidesmall">
 			<p>As a modern-day bakery and restaurant, we strive to provide quality, healthy, and wholesome food for everyone. Finding a reliable source of gluten-free foods, let alone cooking completely gluten-free, can be extremely tough. As experts in the food industry, we know that maintaining a gluten-free lifestyle is challenging. That’s why we founded Panettiere (Italian for "baker") Bakery &amp; Restaurant to provide both delicious food and inspiration to all.</p>
-			<img src="images/features/about_2.png" class="img left">
-			<br><br>
+			<br class="hidesmall"><br class="hidesmall">
+			<img src="images/features/about_2.png" class="img left hidesmall">
+			<br class="hidesmall">
 			<p>At Panettiere, we uphold this standard, and more. We bring high-quality food to the table without neglecting the various dietary needs needs in today's society. By obtaining all of our ingredients fresh from local farmers and producers daily, we ensure the highest quality, as well as purity, of our food. Our bakers and chefs take extreme caution in preparing every pastry and dish that we serve in order to maintain our high standards. </p>
 			<p>Excited about our food yet? Check out our menu <a href="menu.php">here!</a></p>
 		</div>
