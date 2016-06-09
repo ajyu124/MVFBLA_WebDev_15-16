bbb09763dd76420152907e22263fbd9ffbee88a2
William Hsu <will.s.hsu@gmail.com>
2016-06-09 12:43:21 -0700

    revert image

--- header.php
+++ header.php
@@ -74,7 +74,7 @@ if (isset($page)) {
 		<meta property="og:description" content="Panettiere is a modern-day bakery and restaurant that upholds a high standard of quality, healthy, and wholesome food. We bring the best quality gluten-free foods to the table without neglecting various dietary needs in today's society." />
 		<meta property="og:type" content="website" />
 		<meta property="og:url" content="https://panettiere.ml<?php if ($_SERVER['PHP_SELF'] != '/index.php') { echo $_SERVER['PHP_SELF']; } ?>" />
-		<meta property="og:image" content="https://panettiere.ml/images/header/home.jpg" />
+		<meta property="og:image" content="https://panettiere.ml/images/brand/icon_1.png" />
 		<link rel="stylesheet" href="/style.css" />
 		<link rel="icon" href="/images/brand/favicon.ico">
 	</head>
