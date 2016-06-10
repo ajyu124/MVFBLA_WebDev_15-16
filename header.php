6080810ad295f2be09275aa7ee99fb05306cb8c4
William Hsu <will.s.hsu@gmail.com>
2016-06-10 14:19:12 -0700

    added heap tracking

--- header.php
+++ header.php
@@ -76,6 +76,7 @@ if (isset($page)) {
 		<meta property="og:image" content="https://panettiere.ml/images/brand/icon_1.png" />
 		<link rel="stylesheet" href="/style.css" />
 		<link rel="icon" href="/images/brand/favicon.ico">
+		<script type="text/javascript">window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};heap.load("1630246273");</script>
 	</head>
 	<body>
 		<div id="nav">
